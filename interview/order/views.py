37e56b45512edebf3b022b6f6defb930d6168266
Muhammadamirrr <m.amir.dev7@gmail.com>
2024-01-31 14:26:13 -0500

    Challenge 2 Implementation

--- interview/order/views.py
+++ interview/order/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework.views import APIView
+from django.http import JsonResponse, HttpResponseNotAllowed
+from django.shortcuts import get_object_or_404
+
 
 from interview.order.models import Order, OrderTag
 from interview.order.serializers import OrderSerializer, OrderTagSerializer
@@ -13,3 +17,15 @@ class OrderListCreateView(generics.ListCreateAPIView):
 class OrderTagListCreateView(generics.ListCreateAPIView):
     queryset = OrderTag.objects.all()
     serializer_class = OrderTagSerializer
+
+class DeactivateOrderView(APIView):
+    def post(self, request, *args, **kwargs):
+        order_id = kwargs.get('order_id')
+        if not order_id:
+            return JsonResponse({'error': 'Missing order ID'}, status=400)
+
+        order = get_object_or_404(Order, pk=order_id)
+        order.is_active = False
+        order.save()
+
+        return JsonResponse({'message': 'Order deactivated successfully'})
