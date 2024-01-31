d604fc1953f152e2654ce199e7b618b12f0dac22
Muhammadamirrr <m.amir.dev7@gmail.com>
2024-01-31 14:55:06 -0500

    Challenge 4 Integration

--- interview/order/urls.py
+++ interview/order/urls.py
@@ -1,10 +1,10 @@
 
 from django.urls import path
-from interview.order.views import OrderListCreateView, OrderTagListCreateView
+from interview.order.views import OrderListCreateView, OrderTagListCreateView, OrderTagsAPIView
 
 
 urlpatterns = [
     path('tags/', OrderTagListCreateView.as_view(), name='order-detail'),
     path('', OrderListCreateView.as_view(), name='order-list'),
-
+    path('<int:order_id>/tags/', OrderTagsAPIView.as_view(), name='api-order-tags'),
 ]
--- interview/order/views.py
+++ interview/order/views.py
@@ -4,6 +4,11 @@ from rest_framework import generics
 from interview.order.models import Order, OrderTag
 from interview.order.serializers import OrderSerializer, OrderTagSerializer
 
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Order, OrderTag
+
 # Create your views here.
 class OrderListCreateView(generics.ListCreateAPIView):
     queryset = Order.objects.all()
@@ -13,3 +18,15 @@ class OrderListCreateView(generics.ListCreateAPIView):
 class OrderTagListCreateView(generics.ListCreateAPIView):
     queryset = OrderTag.objects.all()
     serializer_class = OrderTagSerializer
+
+class OrderTagsAPIView(APIView):
+
+    def get(self, request, order_id, format=None):
+        try:
+            order = Order.objects.get(id=order_id)
+        except Order.DoesNotExist:
+            return Response({'error': 'Order not found.'}, status=status.HTTP_404_NOT_FOUND)
+
+        tags = order.tags.all()
+        tags_data = [{'id': tag.id, 'name': tag.name} for tag in tags]
+        return Response({'tags': tags_data})
